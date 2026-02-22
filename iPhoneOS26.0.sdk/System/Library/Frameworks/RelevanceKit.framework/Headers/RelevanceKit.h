705b794e777cba0922f6d203926fcdcc45ea4360
Balackburn <allantalbot@protonmail.com>
2026-02-22 04:05:49 +0100

    removed sdks

--- iPhoneOS26.0.sdk/System/Library/Frameworks/RelevanceKit.framework/Headers/RelevanceKit.h
+++
@@ -1,8 +0,0 @@
-//
-//  RelevanceKit.h
-//  RelevanceKit
-//
-//  Created by Yubo Qin on 9/27/24.
-//
-
-#import <Foundation/Foundation.h>
--- iPhoneOS26.1.sdk/System/Library/Frameworks/DeviceDiscoveryUI.framework/Headers/DeviceDiscoveryUI.h
+++
@@ -1,9 +0,0 @@
-//
-//  DeviceDiscoveryUI.h
-//  DeviceDiscoveryUI
-//
-//  Copyright 2022 Apple Inc. All rights reserved.
-//
-
-#import <DeviceDiscoveryUI/DeviceDiscoveryUIDefines.h>
-#import <DeviceDiscoveryUI/DDDevicePickerViewController.h>
